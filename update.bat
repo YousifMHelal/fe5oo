d75474cf354a12fbfb66fbed5b3c022784ffd13d
Yousif Mamdouh <YousifM.Helal@gmail.com>
2026-06-27 17:48:59 +0300

    add the update .bat

--- update.bat
+++ update.bat
@@ -1,11 +1,6 @@
 @echo off
 cd /d "%~dp0"
 
-for /f "delims=" %%b in ('git rev-parse --abbrev-ref HEAD') do set BRANCH=%%b
-echo Pulling origin/%BRANCH%...
-git pull origin %BRANCH%
-if errorlevel 1 ( echo git pull failed & exit /b 1 )
-
 echo Installing dependencies...
 call npm install
 if errorlevel 1 ( echo npm install failed & exit /b 1 )
